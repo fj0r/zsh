0fda07769ebae1498e58261299e9f91715c4dd36
nash <nash@iffy.me>
2021-03-01 21:35:07 +0800

    completion of fabric

--- task.zsh
+++ task.zsh
@@ -4,6 +4,10 @@ if (( $+commands[inv])); then
     __completion_cache invoke "inv --print-completion-script zsh"
 fi
 
+if (( $+commands[fab])); then
+    __completion_cache fabric "fab --print-completion-script zsh"
+fi
+
 if (( $+commands[just])); then
     __completion_cache just "just --completions zsh | sed '\$d'"
     alias j='just'
