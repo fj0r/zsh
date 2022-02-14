53e14b95bf35b189496668b8c85a2324ef4f7345
nash <nash@iffy.me>
2022-02-14 16:58:56 +0800

    drop -> $editor

--- k8s.zsh
+++ k8s.zsh
@@ -3,7 +3,7 @@ if (( $+commands[kubectl] )); then
     source $CFG/.zshrc.d/k8s/kube-ps1.zsh
 
     if [ -n "$VIMRUNTIME" ]; then
-        export KUBE_EDITOR=drop
+        export KUBE_EDITOR=$EDITOR
     else
         export KUBE_EDITOR=$EDITOR
     fi
