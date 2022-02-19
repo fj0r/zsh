a9abc81c95751bfd7640989e65c7749a35863ed8
nash <nash@iffy.me>
2022-02-20 00:41:19 +0800

    awesome-client alert

--- os.zsh
+++ os.zsh
@@ -27,6 +27,45 @@ else
   export route=$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}')
 fi
 
+if (( $+commands[awesome-client]  )); then
+    # send a notification when command completes
+    function alert {
+        RVAL=$?                           # get return value of the last command
+        DATE=`date`                     # get time of completion
+        LAST=$history[$HISTCMD] # get current command
+        LAST=${LAST%[;&|]*}      # remove "; alert" from it
+
+        # set window title so we can get back to it
+        echo -ne "\e]2;$LAST\a"
+
+        LAST=${LAST//\"/'\"'}        # replace " for \" to not break lua format
+
+        # check if the command was successful
+        if [[ $RVAL == 0 ]]; then
+            RVAL="SUCCESS"
+            BG_COLOR="#535d9a"
+        else
+            RVAL="FAILURE"
+            BG_COLOR="#ff2000"
+        fi
+
+        # compose the notification
+        MESSAGE="naughty.notify({ \
+                title = \"Command completed on: \t\t$DATE\", \
+                text = \"$ $LAST\" .. newline .. \"$RVAL\", \
+                timeout = 0, \
+                screen = 2, \
+                bg = \"$BG_COLOR\", \
+                fg = \"#ffffff\", \
+                margin = 8, \
+                width = 382, \
+                run = function () run_or_raise(nill, { name = \"$LAST\" }) end
+                })"
+        # send it to awesome
+        echo $MESSAGE | awesome-client -
+    }
+fi
+
 if [ -n "$WSL_DISTRO_NAME" ]; then
   #ps# Install-Module -Name BurntToast
   toast () { powershell.exe -command New-BurntToastNotification "-Text '$1'" }
