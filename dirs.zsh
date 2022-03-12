9ee958a8292d77a601b915c9e05cf79f9dfbbc63
nash <nash@iffy.me>
2022-03-12 11:00:32 +0800

    fix ~v

--- dirs.zsh
+++ dirs.zsh
@@ -8,7 +8,7 @@ hash -d c="$CFG/.."
 hash -d p="$HOME/pub"
 hash -d s="$HOME/.ssh"
 hash -d k="$HOME/.kube"
-hash -d v="$HOME/.config/nvim"
+hash -d v="${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
 hash -d z="$CFG"
 
 hash -d f="$HOME/Desktop"
